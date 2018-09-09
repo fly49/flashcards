919075e78f528c41ca1300b317efe26303938a2a
flymypie <flymaipie@gmail.com>
2018-09-09 17:56:47 +0000

    Update rails ver

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,17 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180908111021) do
+ActiveRecord::Schema.define(version: 2018_09_08_111021) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "cards", force: :cascade do |t|
-    t.string   "original_text"
-    t.string   "translated_text"
-    t.date     "review_date"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+  create_table "cards", id: :serial, force: :cascade do |t|
+    t.string "original_text"
+    t.string "translated_text"
+    t.date "review_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
