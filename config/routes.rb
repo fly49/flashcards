18688e3dd50e845b76626a4b1a2ee2c4e434ec60
flymypie <flymaipie@gmail.com>
2018-09-10 12:11:26 +0000

    Delete precomp assets & change resorce routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
-  
-  resources :cards
+
+  resources :cards, only: [:index]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
