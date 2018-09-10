e875e6000e9083ee61cb0a0ed519768db15b51c5
flymypie <flymaipie@gmail.com>
2018-09-10 12:28:14 +0000

    Delete seed

--- db/seeds.rb
+++
@@ -1,8 +0,0 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-Card.new(original_text: 'development', translated_text: 'разработка', review_date: Date.today)
