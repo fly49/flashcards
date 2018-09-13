f58433b89d1526cc7ca8845a891bf1de69647003
flymypie <flymaipie@gmail.com>
2018-09-13 10:52:12 +0000

    Embed Adding Cards

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,5 +1,39 @@
 class CardsController < ApplicationController
+  
   def index
     @cards = Card.all
   end
+  
+  def new
+    @card = Card.new
+  end
+  
+  def create
+    @card = Card.new(card_params)
+    if @card.save
+      flash.now[:success] = "Card has been succesfully added!"
+      new
+    else
+      flash.now[:danger] = "Original text and translated text should not be equal!"
+    end
+    render 'new'
+  end
+  
+  def update
+  end
+  
+  def show
+  end
+  
+  def edit
+  end
+  
+  def destoy
+  end
+  
+  private
+  
+    def card_params
+      params.require(:card).permit(:original_text, :translated_text, :review_date)
+    end
 end
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,15 @@
 class Card < ActiveRecord::Base
+  validates :original_text, presence: true, length: { maximum: 20 }
+  validates :translated_text, presence: true, length: { maximum: 50 }
+  validate :texts_not_equal
+  
+  before_save :set_review_date
+  
+  def texts_not_equal
+    errors.add(:translated_text, "can't be the same as original text") if original_text == translated_text
+  end
+  
+  def set_review_date
+    self.review_date = Date.today + 3
+  end
 end
