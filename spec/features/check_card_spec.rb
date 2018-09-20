ade77694dc75e52f289db966df93a875cfd2e5c5
flymypie <flymaipie@gmail.com>
2018-09-20 09:25:23 +0000

    Rubocope specs and use locale

--- spec/features/check_card_spec.rb
+++ spec/features/check_card_spec.rb
@@ -1,26 +1,26 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-feature "Check card" do
-  context "check random card" do
+feature 'Check card' do
+  context 'check random card' do
     let!(:card) { create(:card, :old) }
     before(:each) { visit root_path }
-    
-    scenario "card should be checked successfully" do
-      within("form") do
-        fill_in "Translation", with: card.original_text
+
+    scenario 'card should be checked successfully' do
+      within('form') do
+        fill_in 'Translation', with: card.original_text
       end
-      click_button "Check"
-      expect(page).to have_content('Right!')
-      expect(Card.first.review_date).to eq(Date.today + 3)
+      click_button 'Check'
+      expect(page).to have_content(I18n.t('flashes.successfull_check'))
     end
-    
+
     scenario "card shouldn't been checked" do
-      within("form") do
-        fill_in "Translation", with: "abracadabra"
+      within('form') do
+        fill_in 'Translation', with: 'abracadabra'
       end
-      click_button "Check"
-      expect(page).to have_content('Wrong!')
-      expect(Card.first.review_date).to eq card.review_date
+      click_button 'Check'
+      expect(page).to have_content(I18n.t('flashes.unsuccessfull_check'))
     end
   end
 end
