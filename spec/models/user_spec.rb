88fee83973d788dbf928e87ecb06620920ad0b87
flymypie <flymaipie@gmail.com>
2018-09-21 13:37:16 +0000

    Try travis pass

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,8 +6,13 @@ describe User do
   it { should have_many(:cards) }
 
   describe 'text vaildation' do
+    let(:user) { create(:user) }
+    
     it { should validate_presence_of(:email) }
-    it { should validate_uniqueness_of(:email).ignoring_case_sensitivity }
+    #it { should validate_uniqueness_of(:email).ignoring_case_sensitivity }
+    it "validates uniqueness of email" do
+      expect(user).to validate_uniqueness_of(:email).ignoring_case_sensitivity
+    end
     it { should validate_length_of(:email).is_at_most(100) }
     it { should validate_length_of(:password).is_at_least(6) }
 
