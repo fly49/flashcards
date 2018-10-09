552a1b3140b1507737eb415ff1ececa3a89fd2c8
flymypie <flymaipie@gmail.com>
2018-10-09 14:12:59 +0000

    try again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,8 +86,7 @@ Rails.application.configure do
    :port                 => 587,
    :user_name            => ENV['gmail_username'],
    :password             => ENV['gmail_password'],
-   :authentication       => 'login',
-   :enable_starttls_auto => true,
-   :openssl_verify_mode  => 'none'
+   :authentication       => "plain",
+   :enable_starttls_auto => true
   }
 end
