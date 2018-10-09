6179f22e92d19e36fbc9e77174dd17d348560ef9
flymypie <flymaipie@gmail.com>
2018-10-09 15:44:41 +0000

    revert revert

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,11 +82,12 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
-   :address              => "smtp.gmail.com",
-   :port                 => 587,
-   :user_name            => ENV['gmail_username'],
-   :password             => ENV['gmail_password'],
-   :authentication       => "plain",
-   :enable_starttls_auto => true
+    :user_name => ENV['sendgrid_username'],
+    :password => ENV['sendgrid_password'],
+    :address => 'smtp.sendgrid.net',
+    :domain => 'flymyflashcards.herokuapp.com',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
   }
 end
